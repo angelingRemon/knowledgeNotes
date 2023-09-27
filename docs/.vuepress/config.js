56a4189648678d98203aa5b5ddd6d61eb916b1d7
angelingRemon <790223383@qq.com>
2023-09-27 17:22:24 +0800

    添加了写作辅助

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,7 +58,7 @@ module.exports = {
         ]
       },
       { text: '后端学习', link: '/pages/db78e2/' },
-      { text: '网站导航', link: '/pages/bc7bfe/' },
+      { text: '写作辅助', link: '/pages/bc7bfe/' },
       { text: '优秀资源', link: '/pages/306d53/'}
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
