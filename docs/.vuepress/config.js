e5269195d0a8589055da42d5733005472220e574
RenYongQiang <790223383@qq.com>
2024-12-09 13:42:09 +0800

    更新笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     title: "全菜工程师",
     description: '沉淀与共享优秀知识，构建你的个人笔记和知识管理体系',
-    // base: '/knowledgeNotesPage/',
+    base: '/knowledgeNotes/',
     // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
     head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
         ['link', {rel: 'icon', href: '/img/favicon.ico'}], //favicons，资源放在public文件夹
